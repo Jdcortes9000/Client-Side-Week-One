f1924fa6273c11461eb8c90c60fa376b841586f3
jdcortes@fullsail.edu <jdcortes@fullsail.edu>
2017-06-28 11:31:33 -0400

    basic java practice

---
+++ HTML Template/Scripts/scripts.js
@@ -0,0 +1,16 @@
+var c = console;
+
+window.onload = function ()
+{
+    var elm = document.getElementById("main");
+    log(elm);
+}
+function log(txt)
+{
+    c.log(txt);
+}
+
+log('Hey soul sister');
+log('I dont know the lyrics');
+var elm = document.getElementById("main");
+log(elm);
